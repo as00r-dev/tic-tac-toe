639b137860da5d20977690018238a832a76c514f
as00r <as00r.dev@gmail.com>
2022-12-06 02:38:03 +0530

    Add: basic start menu to finish js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 // Board
 const gameBoard = (function () {
 	const _board = [null, null, null, null, null, null, null, null, null];
+	let lastPlayer = null;
 
-	// Returns "X" or "O" or "UNDECIDED" or "DRAW"
-	const getState = function () {
+	const getBoardCondition = function () {
 		if (_board[0] === _board[4] && _board[4] === _board[8]) {
 			if (_board[0] !== null) return `${_board[0]}`;
 		}
@@ -37,58 +37,96 @@ const gameBoard = (function () {
 
 	const setItemAt = function (choice, position) {
 		_board[position] = choice;
+		_setLastPlayer(choice);
 	};
 
 	const resetState = function () {
 		for (let i = 0; i < 9; i++) {
 			setItemAt(null, i);
 		}
+		lastPlayer = null;
+	};
+
+	const getLastPlayer = function () {
+		return lastPlayer;
+	};
+
+	const _setLastPlayer = function (choice) {
+		lastPlayer = choice;
 	};
 
 	return {
-		getState,
-		setItemAt,
+		getBoardCondition,
+		getLastPlayer,
 		resetState,
+		setItemAt,
 	};
 })();
 
 // Main Game
 const ticTacToe = (function () {
-	let _playerX = {};
-	let _playerO = {};
+	let _playerX;
+	let _playerO;
+
+	let winner;
 
 	const _playerFactory = function (name, choice) {
 		const playMove = (position) => gameBoard.setItemAt(choice, position);
 		return { name, choice, playMove };
 	};
 
-	// Returns false when game is won or draw
-	const isUndecided = function () {
-		return gameBoard.getState() === "UNDECIDED";
+	const _createPlayer = function (name, choice) {
+		if (choice === "O") {
+			_playerO = _playerFactory(name, choice);
+		} else {
+			_playerX = _playerFactory(name, choice);
+		}
 	};
 
-	const currentPlayer = function () {};
+	const getCurrentPlayer = function () {
+		const lastPlayer = gameBoard.getLastPlayer();
+		if (lastPlayer === "X") return _playerO;
+		return _playerX;
+	};
 
-	const createPlayer = function (name, choice) {};
+	const getWinner = function () {
+		return winner;
+	};
 
-	return { createPlayer, currentPlayer, isUndecided };
+	// Returns false when game is won or draw
+	const isUndecided = function () {
+		return gameBoard.getBoardCondition() === "UNDECIDED";
+	};
+
+	const startGame = function (heroName, enemyName, heroChoice, enemyChoice) {
+		_createPlayer(heroName, heroChoice);
+		_createPlayer(enemyName, enemyChoice);
+		gameBoard.resetState();
+	};
+
+	return { getCurrentPlayer, isUndecided, getWinner, startGame };
 })();
 
 // To interact with DOM
-const DOM = (function () {
+const ui = (function () {
 	const _gridBoxes = document.querySelectorAll(".box");
 
+	const _handleGameClick = function (e) {
+		const player = ticTacToe.getCurrentPlayer();
+		// player.playMove(e.target.id);
+		console.log(e.target.id, player);
+	};
+
 	_gridBoxes.forEach((box) => {
-		box.addEventListener("click", _handleClick);
+		box.addEventListener("click", _handleGameClick);
 	});
 
-	function _handleClick(e) {
-		const player = ticTacToe.currentPlayer();
-		player.playMove(player.choice, +e.target.id);
-		if (!ticTacToe.isUndecided) {
-			_declareResult();
-		}
-	}
+	const _handleMatchStartClick = function (e) {
+		// takes names, choice and calls ticTacToe.startGame()
+		// go from main menu to game screen
+	};
 
-	const _declareResult = function () {};
+	const _declareResult = function () {
+		// open a modal declaring winner
+	};
 })();
